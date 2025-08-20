64ea2d3293f6548a6cf9eba9be50589defe0b28b
Oluwabamise <oluwabamise77@gmail.com>
2025-08-20 02:10:28 +0100

    adding site name output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "site_url" {
   description = "The URL of the deployed site"
   value       = "https://${netlify_site.du.name}.netlify.app"
 }
+
+output "site_name" {
+  description = "The site name used"
+  value       = netlify_site.du.name
+}
