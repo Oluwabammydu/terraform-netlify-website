1817e1fe2de45a0a230ffc8d45c783782328816e
Oluwabamise <oluwabamise77@gmail.com>
2025-08-20 01:23:38 +0100

    Removing invalid arguements in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,10 +5,5 @@ output "site_id" {
 
 output "site_url" {
   description = "The URL of the deployed site"
-  value       = "https://${netlify_site.main.default_domain}"
-}
-
-output "admin_url" {
-  description = "The admin URL for the site"
-  value       = netlify_site.main.admin_url
+  value       = "https://${netlify_site.main.url}"
 }
