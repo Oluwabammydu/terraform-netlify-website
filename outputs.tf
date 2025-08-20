ea40532c5262b3e6c29db314d653b74075fe4cba
Oluwabamise <oluwabamise77@gmail.com>
2025-08-20 01:55:20 +0100

    Fix Netlify deployment configuration

--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,12 @@ output "site_id" {
   value       = netlify_site.du.id
 }
 
-# output "site_url" {
-#   description = "The URL of the deployed site"
-#   value       = "https://${netlify_site.du.name}.netlify.app"
-# }
-
 output "site_url" {
   description = "The URL of the deployed site"
-  value       = "https://${netlify_site.du.default_domain}"
+  value       = "https://${netlify_site.du.name}.netlify.app"
 }
+
+# output "site_url" {
+#   description = "The URL of the deployed site"
+#   value       = "https://${netlify_site.du.default_domain}"
+# }
